e144c425d12a0037251e2affdb86cafbc87a7488
Giovanni Pires da Silva <giovannipds@gmail.com>
2021-05-22 12:03:51 -0300

    Write data prototypes

--- linked-list.cpp
+++ linked-list.cpp
@@ -3,3 +3,11 @@ typedef struct lista {
 	int cpf;
 	struct lista *proximo;
 } Dados;
+
+Dados *inicia_dados(char *nome, int cpf);
+Dados *insere_dados(Dados *dados, char *nome, int cpf);
+void exibe_dados(Dados *dados);
+void busca_dados(Dados *dados, char *chave);
+Dados *delete_dados(Dados *dados);
+int checa_vazio(dados *dados);
+
